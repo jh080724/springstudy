153b5d32f94dd4120d6281760429332a93c295c4
jh080724 <tsj010813@gmail.com>
2024-09-30 15:52:05 +0900

    Spring Core 모듈 의존성 관리 학습중(Chap03) - Chap04 실습 중 주석처리한 부분 원복

--- src/main/java/com/study/springstudy/core/chap03/config/AppConfig.java
+++ src/main/java/com/study/springstudy/core/chap03/config/AppConfig.java
@@ -4,39 +4,52 @@ import com.study.springstudy.core.chap03.Chef;
 import com.study.springstudy.core.chap03.JannChef;
 import com.study.springstudy.core.chap03.KimChef;
 import com.study.springstudy.core.chap03.*;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// 객체 생성의 제어권을 모두 가진 객체 AppConfig
+// 스프링에게 객체 생성 등의 제어를 맡길때 사용하는 어노테이션.
+// 수동으로 Bean(객체)를 등록할때 클래스에 선언하는 문법.
+@Configuration
 public class AppConfig {
 
-    // 쉐프 객체 생성
+    @Bean   // 메서드에서 특정 객체가 생성되어 리턴될때 객체의 생명주기를 Spring이 관리하도록 설정.
     public Chef chef1() {
+
         return new KimChef();
     }
 
+    // 쉐프 객체 생성
+    @Bean
     public Chef chef2() {
         return new JannChef();
     }
 
     //요리 코스 객체 생성
+    @Bean
     public Course course1() {
         return new SushiCourse();
     }
 
-    public Course course2(){
+    @Bean
+    public Course course2() {
         return new FrenchCourse();
     }
 
     //레스토랑 객체 생성
+    @Bean
     public Restaurant restaurant1() {
         return new AsianRestaurant(chef1(), course1());
     }
 
+    @Bean
     public Restaurant restaurant2() {
         return new WesternRestaurant(chef2(), course2());
     }
 
     //호텔 객체 생성
-    public Hotel hotel(){
+    @Bean
+    public Hotel hotel() {
         return new Hotel(restaurant1(), chef1());
     }
 

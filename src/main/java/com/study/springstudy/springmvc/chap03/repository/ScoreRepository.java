26b2a8776f6ad388183abfe68060577cc7fabf35
jh080724 <tsj010813@gmail.com>
2024-09-30 17:51:17 +0900

    Spring MVC / Chap03-성적관리 실습 시작

---
+++ src/main/java/com/study/springstudy/springmvc/chap03/repository/ScoreRepository.java
@@ -0,0 +1,17 @@
+package com.study.springstudy.springmvc.chap03.repository;
+
+import com.study.springstudy.springmvc.chap03.entity.Score;
+
+// 역할: 적당한 저장소에 CRUD 담당
+// 예가 자체적으로 하는게 아닌, 기능만 명세 --> 인터페이스니깡
+public interface ScoreRepository {
+    //저장소에 데이터 추가
+    void save(Score score);
+
+    //저장소에서 데이터 전체 조회하기
+
+    //저장소에서 데이터 개벌 조회하기
+
+    // 저장소에서 데이터 삭제하기
+
+}

cd6b16881475cceecbded197cc866879d1834e09
jh080724 <tsj010813@gmail.com>
2024-09-30 15:52:52 +0900

    Spring Core 모듈 의존성 관리 학습중(Chap03) - Chap04 실습 중 수정한 부분 원복

--- src/test/java/com/study/springstudy/core/chap03/HotelDiTest.java
+++ src/test/java/com/study/springstudy/core/chap03/HotelDiTest.java
@@ -2,6 +2,8 @@ package com.study.springstudy.core.chap03;
 
 import com.study.springstudy.core.chap03.config.AppConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +14,20 @@ class HotelDiTest {
 
         AppConfig config = new AppConfig();
         Hotel hotel = config.hotel();
+        hotel.inform();
+
+    }
+
+    @Test
+    void sprintDiTeset(){
+        // 스프링 컨테이너(등록한 빈(객체)을 담다 놓는 컨테이너)를 로딩하는 객체를 생성.
+        // 설정 파일(AppConfig) 클래스 로드
+        AnnotationConfigApplicationContext ctx
+                = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Hotel 클래스 타입으로 등록된 Bean을 리턴받는 메서드.
+        Hotel hotel = ctx.getBean(Hotel.class);
+        // 호켈기능 사용.
         hotel.inform();
 
     }

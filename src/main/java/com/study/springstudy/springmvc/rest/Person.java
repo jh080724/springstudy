6398955be2e4609775d9b743418959285db15287
jh080724 <tsj010813@gmail.com>
2024-10-08 14:02:17 +0900

    Spring MVC / Chap04-게시판 실습 - @RequestBody, @ResponseBody

---
+++ src/main/java/com/study/springstudy/springmvc/rest/Person.java
@@ -0,0 +1,16 @@
+package com.study.springstudy.springmvc.rest;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter @ToString
+@Builder
+public class Person {
+    private String name;
+    private int age;
+    private List<String> hobby;
+
+}

d6ab46fa42a35a9cc8a35e85108da4741b682aee
jh080724 <tsj010813@gmail.com>
2024-10-11 14:30:18 +0900

    Spring MVC / Chap04-게시판 실습 - 쿠키생성/확인

--- src/main/java/com/study/springstudy/springmvc/chap04/controller/MemberController.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.study.springstudy.springmvc.chap04.dto.request.LoginRequestDTO;
 import com.study.springstudy.springmvc.chap04.dto.request.SignUpRequestDTO;
 import com.study.springstudy.springmvc.chap04.service.LoginResult;
 import com.study.springstudy.springmvc.chap04.service.MemberService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -57,19 +59,36 @@ public class MemberController {
                          // Model에 담긴 데이터는 리다이렉트 시 jsp로 전달되지 못함.
                          // 리다이렉트는 응답이 나갔다가 재요청이 들어오는데, 그 과정에서
                          // 응답이 나가는 순간 모델이 소멸함. 그래서, RedirectAttributes를 사용함.
-                         RedirectAttributes ra){
+                         RedirectAttributes ra,
+                         HttpServletResponse response){
 
         LoginResult result = memberService.authenticate(dto);
 
         // redirect에서 데이터를 일회성으로 사용하는 메서드
         ra.addFlashAttribute("result", result);
 
-        if(result == LoginResult.SUCCESS){
-            //로그인 성공
+        if(result == LoginResult.SUCCESS){ //로그인 성공
+            // 로그인을 헸다는 정보를 계속 유지하기 위한 수단으로 쿠키를 사용하자.
+            makeLoginCook(dto, response);
+
             return "redirect:/board/list";
         }
 
         return "redirect:/members/sign-in"; // 로그인 실패 시
+    }
+
+    private void makeLoginCook(LoginRequestDTO dto, HttpServletResponse response) {
+        // 쿠키에 로그인 기록을 저장
+        // 쿠키 객체를 생성 -> 생성자의 매개값으로 쿠키의 이름과 저장할 값을 전달(문자열만 가능 4kb)
+        Cookie cookie = new Cookie("login", dto.getAccount());
+
+        // 쿠키 세부 설정
+        cookie.setMaxAge(60); // 쿠키 수명 설정(초단위)
+        cookie.setPath("/");    // 이 쿠키는 모든 경로에서 유효 - "/"
+
+        // 쿠키가 완성되었다면 응답객체에 쿠키를 태워서 클라이언트로 전송
+        response.addCookie(cookie);
+
     }
 
 }
